e84f10bf7a56722ea7547de9ddcccd7475fcaa91
imrahul05 <rahulkumar20000516@gmail.com>
2025-09-05 12:34:18 +0530

    fix: update hero component image from PNG to GIF for improved visuals

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { MapPin, Mail, Download, ExternalLink, Github, Linkedin, Loader2 } from 'lucide-react';
 import { FaXTwitter } from "react-icons/fa6";
-import img from '../../assets/img1.png';
+import img from '../../assets/img1.gif';
 import { RetroGrid } from '../ui/retro-grid';
 import { AnimatedText } from '../ui/animated-underline-text-one';
 import { BorderBeam } from '../ui/border-beam';
